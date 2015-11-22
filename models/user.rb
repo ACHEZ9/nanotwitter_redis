363d01a902f6ee3c8ce2373e76be3dd2e0b3fd31
Allan Chesarone <allan.chesarone@mobileheartbeart.com>
2015-11-22 17:08:48 -0500

    removing timeline references

--- models/user.rb
+++ models/user.rb
@@ -3,10 +3,7 @@ class User < ActiveRecord::Base
   has_many :followed_users, :through => :follows
   has_many :followings, :class_name => 'Follow', :foreign_key => :followed_user_id
   has_many :followers, :through => :followings, :source => :user
-
-  # has_many :timelines
   has_many :tweets, dependent: :destroy
-  # has_many :feeds, :through => :timelines, :source => :tweet
 
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true, uniqueness: true, length: { minimum: 2 }
